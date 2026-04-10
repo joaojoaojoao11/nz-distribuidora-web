389a3ddd2a9515f7b247b14335811bf72e3120f3
joaojoaojoao11 <joao.vanderleia@gmail.com>
2026-04-09 21:54:31 -0300

    fix: corrigir erros TypeScript para build Vercel

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Hero from '../../components/Hero/Hero';
 import Showcase from '../../components/Showcase/Showcase';
 import About from '../../components/About/About';
--- src/pages/Ppf/Ppf.tsx
+++ src/pages/Ppf/Ppf.tsx
@@ -14,7 +14,7 @@ const staggerContainer = {
 
 const fadeUpItem = {
   hidden: { opacity: 0, y: 30 },
-  show: { opacity: 1, y: 0, transition: { duration: 0.8, ease: [0.22, 1, 0.36, 1] } }
+  show: { opacity: 1, y: 0, transition: { duration: 0.8, ease: [0.22, 1, 0.36, 1] as [number, number, number, number] } }
 };
 
 // Iconografia Premium do Cliente (Simbolos)
